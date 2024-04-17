e9894631275b28188050120a9a6e37c65676bd03
exaby73 <nabeelparkar99@gmail.com>
2024-04-17 22:23:58 +0530

    Initial app setup

---
+++ packages/queuekit_example_app/README.md
@@ -0,0 +1,3 @@
+# queuekit_example_app
+
+A new Flutter project.
---
+++ packages/queuekit_example_app/android/app/src/main/kotlin/com/example/queuekit_example_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.queuekit_example_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
